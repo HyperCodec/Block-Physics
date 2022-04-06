f08d4543e711960d240f11ce48450489649f900e
mrgamer45 <72839119+mrgamer45@users.noreply.github.com>
2022-04-06 17:39:51 -0400

    fixed ram usage for movement

--- src/main/java/me/tristandasavage/physics/Listeners.java
+++ src/main/java/me/tristandasavage/physics/Listeners.java
@@ -71,6 +71,15 @@ public class Listeners implements Listener {
         if(!event.isCancelled() && Main.plugin.getConfig().getInt("autoupdatedistance") != 0 && event.getPlayer().getGameMode() != GameMode.SPECTATOR) {
             UUID uuid = UUID.randomUUID();
             Main.iterations.put(uuid, 0);
+            if(Main.plugin.getConfig().getInt("maxaffectedblocks") != 0) {
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        Main.iterations.remove(uuid);
+                        this.cancel();
+                    }
+                }.runTaskLater(Main.plugin, Main.plugin.getConfig().getInt("maxaffectedblocks") + 20);
+            }
             for(int x = event.getTo().getBlockX() - Main.plugin.getConfig().getInt("autoupdatedistance");x <= event.getTo().getBlockX() + Main.plugin.getConfig().getInt("autoupdatedistance");x++) {
                 for(int y = event.getTo().getBlockY() - Main.plugin.getConfig().getInt("autoupdatedistance");y <= event.getTo().getBlockY() + Main.plugin.getConfig().getInt("autoupdatedistance");y++) {
                     for(int z = event.getTo().getBlockZ() - Main.plugin.getConfig().getInt("autoupdatedistance");z <= event.getTo().getBlockZ() + Main.plugin.getConfig().getInt("autoupdatedistance");z++) {
